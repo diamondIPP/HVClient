9aa00725d8dbc1308084fb3fbdd45214b29d2cec
Gregor Kasieczka <g.kasieczka@gmail.com>
2014-05-25 10:17:30 +0200

    Cleaned up keithleyMain a bit

--- keithleyMain.py
+++ keithleyMain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-""" Main program for Keithley Power Supply steering and readout.
+"""  
+Main program for Keithley Power Supply steering and readout.
 """
 
 
@@ -10,25 +11,18 @@
 
 # Standard Python Modules
 import sys
-import keithleyReader
+import signal
+import time
 import ConfigParser
 import argparse
 import datetime
-
 import Tkinter
-import time
 from threading import Thread
 
 # Command Line Interface
 from keithleyCLI import CLI
+import keithleyReader
 
-
-import signal
-import sys
-def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
 
 #######################################
 # Argument Parser
@@ -45,28 +39,30 @@ config.read(args.config)
 
 
 #######################################
-# Definitions
+# Spawn command line interface
 #######################################
 
-filename_config = "config.txt"
+# The CLI gets its own thread. Tkinter (display GUI) needs to be the
+# main thread.
+
 keithleys = keithleyReader.get_keithleys(config)
-
-
-#######################################
-# Spawn line interface
-#######################################
-
 myCLI = CLI()
 myCLI.set_keithleys(keithleys)
 myCLI.start()
 
+
+#######################################
+# Signal handling
+# (make sure we can kill the GUI with
+#   ctrl+c)
+#######################################
+
 def signal_handler(signal, frame):
-    print('You pressed Ctrl+C!')
+    print 'Received SIGINT'
     myCLI.stop()
     sys.exit(0)
 
-
-
+signal.signal(signal.SIGINT, signal_handler)
 
 
 #######################################
@@ -74,25 +70,38 @@ def signal_handler(signal, frame):
 #######################################
 
 root = Tkinter.Tk()
-root.minsize(1000, 200)
+root.minsize(1000, 200) # x/y
 root.maxsize(1000, 200)
 
+# Create Tk-variable and -label objects for each keithley
+# A label is an object and the variable is the content
 di_vars   = {}
 di_labels = {}
 
+# create labels and hook them up with variables
 for name in keithleys.keys():
     tmp = Tkinter.StringVar()
     di_vars[name] = tmp
     di_labels[name] = Tkinter.Label(root, textvariable = tmp)
 
+# add the labels to the output window
 for k in sorted(di_labels.keys()):
     di_labels[k].pack()
+
+
+#######################################
+# Main GUI loop
+#######################################
+
 now = time.time()
 while myCLI.running:
+    
+    # Make sure enough time has passed before we poll again
     while time.time()-now < 1:
-            time.sleep(.05)
+        time.sleep(.05)
     now = time.time()
 
+    # Loop over the keithleys, get the voltages and update the display
     for k,v in sorted(keithleys.iteritems(), key = lambda x:x[0]):
         v.wait_for_device()
         v.isBusy=True
@@ -108,3 +117,5 @@ while myCLI.running:
             di_vars[k].set( k+": OFF")
         v.isBusy=False
         root.update()
+    # end of Keithley loop        
+# End of Main GUI loop
