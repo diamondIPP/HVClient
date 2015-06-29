71d1558950f32d564f59ea0add0ea7363ea8f827
veloxid <bachmair@phys.ethz.ch>
2015-06-29 16:22:26 +0200

    added IDN? query

--- Keithley2657.py
+++ Keithley2657.py
@@ -6,7 +6,7 @@ __author__ = 'bachmair'
 # IMPORTS
 # ============================
 import ConfigParser
-import serial
+import visa
 from HV_interface import HVInterface
 from time import sleep,time
 
@@ -23,7 +23,25 @@ OFF = 0
 class Keithley2657(HVInterface):
     def __init__(self, config, device_no=1, hot_start=False):
         HVInterface.__init__(self, config, device_no, hot_start)
+        self.bOpen = False
+        self.ip_address = config.get(self.section_name, 'ip_address')
+        self.rm = visa.ResourceManager('@py')
+        self.inst = None
+        self.identifier = None
+        self.answer_time = 0.1
+        self.open_tcp_connection()
+        self.init_keithley(hot_start)
 
 
     def open_tcp_connection(self):
-        pass
+        resource_name = "TCPIP::%s::INSTR"%self.ip_address
+        self.inst = self.rm.open_resource(resource_name)
+
+    def self_identifier(self):
+        return self.inst.query('*IDN?')
+
+
+if __name__ == '__main__':
+    conf = ConfigParser.ConfigParser()
+    conf.read('keithley.cfg')
+    k2657 = Keithley2657(conf, 4, False)
