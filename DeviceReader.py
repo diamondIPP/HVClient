8c37ae66147ed9e3d6efa87b3f92e3a7a02bfc9d
Felix BAchmair <bachmair@phys.ethz.ch>
2015-07-01 13:17:56 +0200

    switched to reading an array of devices

--- DeviceReader.py
+++ DeviceReader.py
@@ -3,23 +3,24 @@
 # ============================
 from HV_Device import HVDevice
 from ConfigParser import NoOptionError
+import json
 
 
 def get_devices(config, hot_start):
     devices = {}
     try:
-        n_devices = config.getint('Main', 'n_devices')
+        device_numbers = json.loads(config.get('Main','devices'))
+        print device_numbers
     except NoOptionError, err:
         print err
-        n_devices = 0
     print '\nOptions from config file:'
     main = config.sections()[0]
     print main, config.options(main)
-    print 'Found %d devices in config file' % n_devices
-    for i in range(1, n_devices + 1):
+    print 'Found %d devices in config file: %s' % (len(device_numbers), device_numbers)
+    for i in device_numbers:
         sec = 'HV' + str(i)
         print sec, config.options(sec)
-    for i in range(1, n_devices + 1):
+    for i in device_numbers:
         name = 'HV%d' % i
         if config.has_section(name):
             devices[name] = HVDevice(config, i, hot_start)
