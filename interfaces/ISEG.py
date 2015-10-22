c3831bce2e71c8e4dc423bb8c68cfb566d30554e
Felix Bachmair <bachmair@phys.ethz.ch>
2015-10-22 22:28:54 +0200

    started to implement functions of class

--- interfaces/ISEG.py
+++ interfaces/ISEG.py
@@ -103,6 +103,48 @@ class ISEG(HVInterface):
         self.identifier = self.get_answer_for_query('*IDN?')
         self.get_model_name()
 
+    def is_valid_voltage(self,voltage,channel):
+        if channel < 0 or channel > 5:
+            raise AttributeError('Invalid voltage ')
+        # todo - write function
+        return True
+
+    def is_valid_voltage_bound(self,voltage,channel):
+        # todo - write function
+        return True
+
+    def is_valid_current(self,current,channel):
+        #todo
+        return True
+
+    def is_valid_current_bound(self,current_bound,channel):
+        # todo
+        return True
+
+    def is_valid_channel_string(self,channel):
+        channel = str(channel)
+        if channel == 'all':
+            return True
+        try:
+            ch = int(channel)
+            if 0<=ch <= 5:
+                return True
+        except:
+            pass
+        raise AttributeError('invalid channel no %s'%channel)
+
+    def get_channel_string(self, channel):
+        if channel == 'all'
+            return '0-5'
+        if not type(channel)==list:
+            channel = [channel]
+        valid_channels =  [0<=ch <= 5 for ch in channel]
+        if not all(valid_channels):
+            raise AttributeError('Invalid channel in list')
+        channel = [str(x) for x in channel]
+            return ','.join(channel)
+
+
     # ============================
     # DEVICE FUNCTIONS
     def set_output(self, status, channel='all'):
@@ -118,6 +160,118 @@ class ISEG(HVInterface):
         print_value += ('ON' if status else 'OFF')
         print print_value
         return self.write(data)
+
+    def set_channel_voltage(self,voltage,channel=-1):
+        self.is_valid_channel_string(channel)
+        self.is_valid_voltage(voltage,channel)
+        data = ':VOLT %.3f (@%d)'%(voltage,channel)
+        print 'set Voltage of channel %d to %.3f V'%(channel,voltage)
+        return self.write(data)
+
+    def set_emergency_off(self,channel='all'):
+        self.is_valid_channel_string(channel)
+        print 'Emergency off for channel(s) %s'%channel
+        data = ':VOLT EMCY OFF(@ch%s)'%channel
+        return self.write(data)
+
+    def set_emergency_clear(self,channel='all'):
+        self.is_valid_channel_string(channel)
+        print 'Emergency clear for channel(s) %s'%channel
+        data = ':VOLT EMCY CLR(@ch%s)'%channel
+        return self.write(data)
+
+    def set_channel_voltage_bound(self,voltage_bound,channel=-1):
+        self.is_valid_channel_string(channel)
+        self.is_valid_voltage_bound(voltage_bound,channel)
+        data = ':VOLT:BOU %.3f (@%s)'%(voltage_bound,channel)
+        return self.write(data)
+
+    def set_channel_current(self,current,channel=-1):
+        self.is_valid_channel_string(channel)
+        self.is_valid_current(current,channel)
+        data = ':CURR %.3f (@%d)'%(current,channel)
+        print 'set Current of channel %d to %.3f V'%(channel,current)
+        return self.write(data)
+
+    def set_channel_current_bound(self,current_bound,channel=-1):
+        self.is_valid_channel_string(channel)
+        self.is_valid_current_bound(current_bound,channel)
+        data = ':CURR:BOU %.3e (@%s)'%(current_bound,channel)
+        print 'set current bound of %d to %.3e A'%(channel,current_bound)
+        return self.write(data)
+
+    def clear_channel_events(self,channel=-1):
+        self.is_valid_channel_string(channel)
+        data = ':EV:CLEAR (@%s)'%channel
+        print 'Clear channel events @ch'%channel
+        return self.write(data)
+
+    def set_channel_event_mask(self,mask_word,channel=-1):
+        self.is_valid_channel_string(channel)
+        self.is_valid_channel_mask(mask_word)
+        print 'set channel event mask for ch %s to "%s"'%(channel,bin(mask_word))
+        data = ':EV:MASK %s'%mask_word
+        return self.write(data)
+    # todo :CONFIGURE:TRIP
+    # todo :CONFIGURE:INH
+
+    def configure_ramp_speed(self,speed,ramp_type):
+        if len(ramp_type) > 4:
+            ramp_type = ramp_type[:4]
+        ramp_type = ramp_type.upper()
+        if not ramp_type in ['VOLT','CURR']
+            raise AttributeError('Invalid Type of ramp speed')
+        if not 0< speed <= 100:
+            AttributeError('Invalid speed for ramp speed')
+        data = ':CONF:RAMP:%s %.1f'%(ramp_type,speed)
+        print 'Set %s ramp speed to %.1f %/s'%(ramp_type,speed)
+        return self.write(data)
+
+    def configure_ramp_speed_voltage(self,speed):
+        self.configure_ramp_speed(speed,'VOLT')
+
+    def configure_ramp_speed_current(self,speed):
+        self.configure_ramp_speed(speed,'CURR')
+
+    def configure_average(self,filterSteps):
+        valid_steps = [1,16,64,256,512,1024]
+        if not filterSteps in valid_steps:
+            raise AttributeError('FilterSteps must be in %s'%valid_steps)
+        data = ':CONF:AVER %d'%filterSteps
+        print 'Set Average Filter Steps to %d'filterSteps
+        return self.write(data)
+
+    def get_average_filter_steps(self):
+        return int(self.get_answer_for_query(':CONF:AVER?'))
+
+    def set_kill_function(self,value):
+        value = bool(value)
+        data = ':CONF:KILL %s'
+        if value:
+            retval = 'ENABLE'
+        else:
+            retval = 'DISABLE'
+        print '%s kill function'%retval.lower()
+        return self.write(data%retval)
+
+    def is_kill_function_active(self):
+        retval = self.get_answer_for_query(':CONF:KILL?')
+        return bool(retval)
+
+    #todo :CONF:EV:CLEAR
+    #todo :CONF:EV <WORD>
+    #todo :CONF:EV:MASK
+    #todo :CONF:EV:CHANMASK
+
+    def read_current(self,channel=-1):
+        retVal = (self.get_answer_for_query(':MEAS:CURR? (@%s)')%self.get_channel_string(channel)).split()
+        retVal = [k[:-1] for k in retVal]
+        return retVal
+
+    def read_voltage(self,channel=-1):
+        retVal = (self.get_answer_for_query(':MEAS:CURR? (@%s)')%self.get_channel_string(channel)).split()
+        retVal = [k[:-1] for k in retVal]
+        return retVal
 
     def reset(self):
         self.write('*RST')
