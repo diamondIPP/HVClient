af9c2b0ee1ef6738d791a7dd34872b3e5575e398
Felix Bachmair <bachmair@phys.ethz.ch>
2015-10-30 15:14:33 +0100

    only add active channels of iseg device

--- DeviceReader.py
+++ DeviceReader.py
@@ -30,7 +30,7 @@ def get_devices(config, hot_start):
             if config.get(name,'model').startswith('NHS'):
                 print 'special module with multiple channels'
                 module = ISEG(config,i,hot_start)
-                for ch in range(module.query_module_channels()):
+                for ch in module.get_list_of_active_channels():
                     devices[name+'_CH%d'%ch] = HVDevice(config,i,hot_start,module=module,channel=ch)
             else:
                 devices[name] = HVDevice(config, i, hot_start)
