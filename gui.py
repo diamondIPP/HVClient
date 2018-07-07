2738347f3e7a55d038db917cb90cc598b0324df2
michareichmann <remichae@phys.ethz.ch>
2018-07-07 04:22:59 +0200

    add new gui class

--- gui.py
+++ gui.py
@@ -1,118 +1,79 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # --------------------------------------------------------
 #       GUI for the ETH High Voltage Client
 # created on June 29th 2018 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from PyQt4.QtGui import QMainWindow, QIcon, QApplication, QStyleFactory, QAction, QFontDialog, QGroupBox, QGridLayout, QLabel, QLineEdit, QTextEdit, QCheckBox, QVBoxLayout, \
-    QPushButton, QFont
-from PyQt4.QtCore import SIGNAL, Qt
+from PyQt4.QtGui import QMainWindow, QIcon, QApplication, QAction, QFontDialog, QVBoxLayout, QWidget
+from PyQt4.QtCore import QTimer
 from sys import exit as end
 from ConfigParser import ConfigParser
 from DeviceReader import get_devices
-from functools import partial
+from devices.Keithley24XX import Keithley24XX
+from devices.ISEG import ISEG
+from os.path import dirname, realpath, join
+from DeviceBox import DeviceBox
+
 
 ON = True
 OFF = False
+
+
+# todo: add auto setting for max/min current
+# todo: add plus/min slider slightly overlapping
 
 
 class Gui(QMainWindow):
     def __init__(self, devices):
         super(Gui, self).__init__()
 
+        self.Dir = dirname(realpath(__file__))
+
+        # Devices
         self.Devices = devices
-        devices.values()[0].start()
-        self.NDevices = len(devices)
+        self.start_threads()
+        self.CurrentDevice = self.Devices[0]
+        self.CurrentChannel = 0
+        self.NDevices = sum(len(device.ActiveChannels) for device in devices)
+
+        self.MainBox = QVBoxLayout()
         self.configure()
         self.MenuBar = MenuBar(self)
-        self.IsUpdating = True
 
-        self.make_device_box(devices.keys()[0], devices.values()[0], 'CH0')
+        self.DeviceBoxes = self.make_device_boxes()
+
+        self.timer = QTimer()  #updates the plot
+        self.timer.timeout.connect(self.update)
+        self.timer.start(500)
 
         self.show()
 
+    def closeEvent(self, QCloseEvent):
+        self.MenuBar.close_app()
+
+    def update(self):
+        for device_box in self.DeviceBoxes:
+            device_box.update()
+
     def configure(self):
-        self.setGeometry(500, 300, 1000, 100 + self.NDevices * 300)
+        self.setGeometry(2000, 300, 1000, 100 + self.NDevices * 300)
         self.setWindowTitle('ETH High Voltage Client')
-        self.setWindowIcon(QIcon('Pics/icon.svg'))
-        QApplication.setStyle(QStyleFactory.create('Plastique'))
+        self.setWindowIcon(QIcon(join(self.Dir, 'Pics', 'icon.svg')))
+        self.setCentralWidget(QWidget())
+        self.centralWidget().setLayout(self.MainBox)
 
-    def make_device_box(self, name, device, channel):
-        box = QGroupBox(device.read_device_name(), self)
-        f = QFont()
-        f.setBold(True)
-        f.setPointSize(20)
-        box.setFont(f)
-        layout = QGridLayout(box)
-        layout.setContentsMargins(4, 4, 4, 4)
+    def start_threads(self):
+        for device in self.Devices:
+            device.start()
 
-        bias_field = make_line_edit('0')
-        bias_button = create_bias_button(bias_field, device, channel)
-
-        layout.addWidget(create_on_button(device, channel), 1, 0, 1, 2, Qt.AlignCenter)
-        layout.addWidget(bias_field, 2, 1)
-        layout.addWidget(bias_button, 2, 0)
-        layout.addWidget(QLabel('Displayed Hours'), 3, 0)
-        layout.addWidget(QLabel('Max Current'), 4, 0)
-        layout.addWidget(QLabel('Min Current'), 5, 0)
-        layout.addWidget(QLabel('Current Unit'), 6, 0)
-        layout.addWidget(self.create_break_button(), 7, 0, 1, 2, Qt.AlignCenter)
-        smallEditor = QTextEdit()
-        smallEditor.setPlainText('Here should be a plot ...')
-        layout.addWidget(smallEditor, 0, 2, 8, 1)
-        layout.setColumnStretch(1, 10)
-        layout.setColumnStretch(2, 50)
-        for widget in box.children():
-            try:
-                widget.setFont(QFont('arial', 15))
-            except:
-                pass
-        box.setLayout(layout)
-        self.setCentralWidget(box)
-
-    def create_break_button(self):
-        button = make_button('Break Update')
-
-        def f():
-            button.setText('{} Update'.format('Continue' if self.IsUpdating else 'Break'))
-            self.IsUpdating = not self.IsUpdating
-
-        button.clicked.connect(f)
-        return button
-
-
-def create_bias_button(obj, device, channel):
-    button = make_button('Set Bias')
-
-    def f():
-        device.set_target_bias(int(obj.text()), channel)
-
-    button.clicked.connect(f)
-    return button
-
-
-def create_on_button(device, channel):
-    button = make_button('OFF' if device.get_status(channel) else 'ON')
-
-    def change_output():
-        device.power_down(channel) if device.get_status(channel) else device.interface.set_output('ON', channel)
-        button.setText('ON' if device.get_status(channel) else 'OFF')
-
-    button.clicked.connect(change_output)
-    return button
-
-
-def make_line_edit(txt=''):
-    line_edit = QLineEdit()
-    line_edit.setText('0')
-    return line_edit
-
-
-def make_button(txt, size=200):
-    but = QPushButton()
-    but.setText(txt)
-    but.setFixedWidth(size)
-    return but
+    def make_device_boxes(self):
+        boxes = []
+        for device in self.Devices:
+            for channel in device.ActiveChannels:
+                boxes.append(DeviceBox(device, channel))
+                self.MainBox.addWidget(boxes[-1])
+        return boxes
 
 
 class MenuBar(object):
@@ -145,16 +106,21 @@ class MenuBar(object):
 
     def close_app(self):
         print 'Closing application'
-        for dev in self.Window.Devices.itervalues():
-            dev.isKilled = True
+        for dev in self.Window.Devices:
+            dev.IsKilled = True
         end(2)
 
 
 if __name__ == '__main__':
-    config = ConfigParser()
-    config.read('config/keithley.cfg')
-    devices = get_devices(config, True)
 
+    config = ConfigParser()
+    config.read(join(dirname(realpath(__file__)), 'config', 'keithley.cfg'))
+
+    # devices = get_devices(config, False)
+    devices = [ISEG(config, 3, True)]
+    print(devices[0].ActiveChannels)
+    # devices = [Keithley24XX(config, 1, True)]
     app = QApplication([5])
+    app.setStyle('Macintosh')
     g = Gui(devices)
     end(app.exec_())
